8c839b6f6af3fe9b7bf03c8171997552b59923af
ConduracheGF <condurachegabriel14@gmail.com>
2024-10-08 10:02:18 +0300

    Update header.h

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+void Grafica_TicTac(char *spatiu);
+void Move_Player_1(char *spatiu, char player_1);
+void Move_Player_2(char *spatiu,char player_2);
+bool castigator(char *spatiu,char player_1, char player_2);
+bool verif_Tie(char *spatiu);
+
+#endif
